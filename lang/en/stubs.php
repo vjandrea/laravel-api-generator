cc5828b60442a1b94d4efb8f787b98203625dfb6
Andrea Bergamasco <info@vjandrea.net>
2015-10-20 20:42:27 +0200

    Translated stubs

---
+++ lang/en/stubs.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Strings used in templates
+ */
+
+return [
+    'save'                  => 'Save',
+    'no_models_found'        => 'No :model_name found.',
+    'add_new'               => 'Add New',
+    'confirm_delete_model'  => 'Are you sure wants to delete this :model_name?',
+    'action'                => 'Action',
+    'model_saved'           => ':model_name saved successfully.'
+    'model_not_found'       => ':model_name not found.',
+    'model_updated'         => ':model_name updated successfully.',
+    'model_deleted'         => ':model_name deleted successfully.'
+];
